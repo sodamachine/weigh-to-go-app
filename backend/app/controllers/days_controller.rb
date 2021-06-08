5c06f20e74654e4691324bfc41614571e4405998
Stephanie <stephanie.ohn@gmail.com>
2021-06-08 11:35:03 -0400

    adds show DaysController action

--- backend/app/controllers/days_controller.rb
+++ backend/app/controllers/days_controller.rb
@@ -3,5 +3,10 @@ class DaysController < ApplicationController
     def index
         render json: Day.all
     end
-    
+
+    def show
+        day = Day.find_by(id: params[:id])
+        render json: day
+    end
+
 end
