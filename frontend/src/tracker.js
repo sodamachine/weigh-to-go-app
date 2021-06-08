4073f3b3852238d8b4e11e89b69bd7105b1bdff5
Stephanie <stephanie.ohn@gmail.com>
2021-06-08 08:24:07 -0400

    adds appendTrackers fn

--- frontend/src/tracker.js
+++ frontend/src/tracker.js
@@ -1,9 +1,27 @@
 class Tracker {
 
+    appendTracker(){
+        const trackersDiv = document.getElementById("trackers")
+        const li = document.createElement("li")
+        const div = document.createElement("div")
+        li.innerText = this.name
+        li.addEventListener('click', this.showTrackerPage.bind(this))
+        trackersDiv.append(div)
+        div.append(li)
+        this.appendRecords(div)
+    }
+    
     static fetchTrackers(){
         fetch("http://localhost:3000/trackers")
         .then(jsonToJS)
         .then(this.appendTrackers)
+    }
+
+    static appendTrackers(trackers){
+        for (let tracker of trackers){
+            let newTracker = new Tracker(tracker)
+            newTracker.appendTracker()
+        }
     }
 
 }
