280eb31a4323b6a59a77fd9ecdf1ae5044d5e21b
Stephanie <stephanie.ohn@gmail.com>
2021-06-08 11:30:38 -0400

    adds has_many macro to Day model

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Day.create(date: "06/01/2021")
+Day.create(date: "06/02/2021")
+
+Record.create(weight: 140)
+Record.create(weight: 139)
