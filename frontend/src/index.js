edde51e396ad92987b9c048ab0d5dc794a5c268f
Stephanie <stephanie.ohn@gmail.com>
2021-06-08 16:00:44 -0400

    defines returnHome for Home button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,3 +7,15 @@ function jsonToJS(resp) {
     return resp.json()
 }
 
+function returnHome() {
+    document.getElementById('dayContainer').innerHTML = `
+        <form id="dayForm">
+            <label>Log your day</label>
+            <input type="text" id="datepicker">
+            <input type="submit" value="Submit">
+        </form>
+        <div id="days"></div>`
+    dayForm = document.getElementById("dayForm")
+    dayForm.addEventListener("submit", Day.postDay)
+    Day.appendListsOnReturnHome()
+}
