eccce0286f7634f944a04e984fa6a94b0cdf8199
Stephanie <stephanie.ohn@gmail.com>
2021-06-09 05:52:57 -0400

    updates code to render calendar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,94 @@
-// let dayForm = document.getElementById('dayForm')
-// dayForm.addEventListener('submit', Day.postDay)
+// // let dayForm = document.getElementById('dayForm')
+// // dayForm.addEventListener('submit', Day.postDay)
 
-// Day.fetchDays()
+let monthList = new Array(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+)
 
-Calendar.appendCalendar()
+let dayList = new Array(
+    "SUN",
+    "MON",
+    "TUE",
+    "WED",
+    "THU",
+    "FRI",
+    "SAT"
+)
 
-function jsonToJS(resp) {
-    return resp.json()
+let today = new Date()
+
+// // let currentMonth = new Date(
+// //     today.getFullYear(), 
+// //     today.getMonth(), 
+//     // 1
+// // )
+
+let monthLength = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    0
+).getDate()
+
+const calendar = document.getElementById("calendar")
+
+function appendMonth(){
+    let header = document.createElement("div")
+    let month = document.createElement("div")
+    header.classList.add("header")
+    month.classList.add("month")
+    month.innerText = monthList[today.getMonth()] + " " + today.getFullYear()
+    header.appendChild(month)
+    calendar.appendChild(header)
+    appendWeekdays()
 }
+
+function appendWeekdays(){
+    let weekday = document.createElement("div")
+    for (let i = 0; i < dayList.length; i++){
+        let cell = document.createElement("span")
+        cell.classList.add("cell")
+        cell.classList.add("day")
+        cell.innerText = dayList[i].substring(0, 3)
+        weekday.appendChild(cell)
+    }
+    for (let i = 0; i < today.getDay(); i++){
+        let cell = document.createElement("span")
+        cell.classList.add("cell")
+        cell.classList.add("empty")
+        weekday.appendChild(cell)
+    }
+    for (let i = 1; i <= monthLength; i++){
+        let cell = document.createElement("span")
+        cell.classList.add("cell")
+        cell.innerText = `${i}`
+        weekday.appendChild(cell)
+    }
+    calendar.appendChild(weekday)
+}
+
+
+    
+    
+    // calendar.appendChild(day)
+
+
+
+
+// function jsonToJS(resp) {
+//     return resp.json()
+// }
+
+appendMonth()
+
+// // Tracker.fetchTrackers()
---
+++ frontend/src/tracker.js
@@ -0,0 +1,20 @@
+// class Tracker {
+
+//     constructor({name, id, records}){
+//         this.name = name
+//         this.id = id
+//         this.records = records.map(record => new Record(record))
+//     }
+
+//     static fetchTrackers(){
+//         fetch("http://localhost:3000/trackers")
+//         .then(jsonToJS)
+//         .then(this.appendCalendar)
+//     }
+
+    
+
+
+
+
+// }
