211417138ae540ae2c96d8afa422800ec75ace7e
Stephanie <stephanie.ohn@gmail.com>
2021-06-09 00:48:38 -0400

    defines appendDays

--- frontend/src/calendar.js
+++ frontend/src/calendar.js
@@ -24,6 +24,12 @@ let monthList = new Array(
   )
 
   let calendarDiv = document.getElementById("calendar")
+  let today = new Date()
+  let monthLength = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    0
+)
   
   class Calendar {
 
@@ -41,22 +47,49 @@ let monthList = new Array(
     }
 
     static appendMonth(headerDiv){
-        let today = new Date()
         // // let currentMonth = new Date(
         //     today.getFullYear(), 
         //     today.getMonth(), 1
         //     )
         let monthDiv = document.createElement("div")
-        // let monthLength = new Date(
-        //     today.getFullYear(),
-        //     today.getMonth() + 1,
-        //     0
-        // )
         monthDiv.classList.add("month")
         monthDiv.innerText = monthList[today.getMonth()] + " " + today.getFullYear()
         headerDiv.append(monthDiv)
-        this.appendWeekdays(today)
+        this.appendWeekdays()
     }
+
+    static appendWeekdays(){
+        let weekdayDiv = document.createElement("div")
+        for (let i = 0; i < dayList.length; i++){
+            let weekdaySpan = document.createElement("span")
+            weekdaySpan.classList.add("cell")
+            weekdaySpan.classList.add("day")
+            weekdaySpan.innerText = dayList [i].substring(0, 3)
+            weekdayDiv.append(weekdaySpan)
+            calendarDiv.append(weekdayDiv)
+        }
+        // for (let i = 0; i < today.getDay(); i++){
+        //     let daySpan = document.createElement("span")
+        //     daySpan.classList.add("cell")
+        //     daySpan.classList.add("empty")
+        // }
+        calendarDiv.append(weekdayDiv)
+        this.appendDays()
+    }
+
+    static appendDays(){
+        let dayDiv = document.createElement("div")
+        for (let i = 1; i <= monthLength; i++){
+            let daySpan = document.createElement("span")
+            daySpan.classList.add("cell")
+            daySpan.innerText = `${i}`
+            dayDiv.append(daySpan)
+        }
+        calendarDiv.append(dayDiv)
+        this.append
+    }
+
+
 
 
 
