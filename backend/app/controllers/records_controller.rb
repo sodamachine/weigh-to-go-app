34f683b007bf583977cb4fa8544d10710d6476bb
Stephanie <stephanie.ohn@gmail.com>
2021-06-09 03:37:15 -0400

    adds controllers actions index + show

--- backend/app/controllers/records_controller.rb
+++ backend/app/controllers/records_controller.rb
@@ -1,2 +1,12 @@
 class RecordsController < ApplicationController
+
+    def index
+        render json: Record.all
+    end
+
+    def show
+        record = Record.find_by(id: params[:id])
+        render json: record
+    end
+
 end
