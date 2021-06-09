e718581a276d6c7a70cea20fb1c3300374d4f8d0
Stephanie <stephanie.ohn@gmail.com>
2021-06-09 03:28:01 -0400

    sets up db tables

--- backend/db/migrate/20210609072320_create_trackers.rb
+++ backend/db/migrate/20210609072320_create_trackers.rb
@@ -1,6 +1,7 @@
 class CreateTrackers < ActiveRecord::Migration[6.1]
   def change
     create_table :trackers do |t|
+      t.string :name
 
       t.timestamps
     end
--- backend/db/migrate/20210609072401_create_records.rb
+++ backend/db/migrate/20210609072401_create_records.rb
@@ -1,6 +1,8 @@
 class CreateRecords < ActiveRecord::Migration[6.1]
   def change
     create_table :records do |t|
+      t.integer :num
+      t.string :unit
       t.belongs_to :tracker, null: false, foreign_key: true
 
       t.timestamps
